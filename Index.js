b0cc323b88e66fe41751051bde9a73f08336ef5e
Jeff Crawford <jeffrey.a.crawford@gmail.com>
2018-04-14 06:56:39 -0400

    everything but extra enter push

--- Index.js
+++ Index.js
@@ -1,39 +1,28 @@
-/* EXTERNAL DOCUMENT VARIABLES */
 var Word = require("./word.js");
 var Letter = require("./letter.js");
 var inquirer = require("inquirer");
 
 
-/* WORDLIST VARIABLES */
 var wordList = ["astrophysics", "astronomy", "atom", "beaker", "biochemistry", "biology", "botany", "cell", "chemical", "chemistry", "climate", "climatologist", "control", "cuvette", "data", "datum", "electricity", "electrochemist", "element", "energy", "entomology", "evolution", "experiment", "fact", "flask", "fossil", "funnel", "genetics", "geology", "geophysics", "glassware", "gravity", "herpetology", "hypothesis", "ichthyology", "immunology", "lab", "laboratory", "laws", "lepidoptery", "magnetism", "mass", "matter", "measure", "meteorologist", "meteorology", "microbiologist", "microbiology", "microscope", "mineral", "mineralogy", "molecule", "motion", "observe", "observatory", "organism", "ornithology", "paleontology", "particle", "phase", "physics", "pipette", "radiology", "research", "retort", "scale", "science", "scientist", "seismology", "telescope", "temperature", "theory", "thermometer", "tissue", "variable", "virologist", "volcanology", "volume", "weather", "weigh", "zoology"];
 var wordSelection = function() {
     return wordList[Math.floor(Math.random()*wordList.length)];
 }
 
-/* NEW WORD OBJECT VARIABLES */
 var text = wordSelection().toString();
 var letterArray = [];
-
 var newWord = new Word(text, letterArray);
 
 
-/* CYCLES THROUGH THE SELECTED WORD STRING AND GENERATES AN ARRAY OF LETTER OBJECTS */
 var fillArray = function() {
     for (i = 0; i < text.length; i++) {
         var newLetter = new Letter(text[i])
         letterArray.push(newLetter);
     }
 }
-fillArray();
 
 
-/* INPUT VARIABLES USED IN BEGIN FUNCTION */
 var input = process.argv[2];
 var count = 0;
-
-
-/* DISPLAYS INITIAL _ STRING */
-
 
 
 var reset = function() {
@@ -43,6 +32,7 @@ var reset = function() {
     newWord = new Word(text, letterArray);
     fillArray();
     newWord.returnString();
+    console.log(newWord.text)
     begin();
 }
 
@@ -60,6 +50,14 @@ var begin = function() {
             if (newWord.text.includes(input.guess.toLowerCase())) {
                 console.log("\n");
                 console.log("CORRECT!")
+
+                var checkProgress = function() {
+                    if (newWord.text === newWord.letterArray.join("")) {
+                        console.log("YOU WIN")
+                        playAgain();
+                    }
+                }
+                checkProgress();
             }
             /* IF THE WORD DOES NOT INCLUDE THE GUESS, LOG WRONG AND ADD TO THE MISS COUNT */
             else {
@@ -71,7 +69,8 @@ var begin = function() {
             }
             console.log("\n");
             newWord.returnString(input.guess.toLowerCase());
-             
+
+
             begin();
             
         })
@@ -80,21 +79,27 @@ var begin = function() {
         console.log("\n");
         console.log("YOU LOSE!")
         wordSelection();
-        inquirer.prompt([
-            {
-            name: "restart",
-            type: "confirm",
-            message: "Play Again?"
-            }
-        ]).then(function(input) {
-            if (input.restart) {
-                reset();
-            }
-            else {
-                console.log("GAME OVER")
-            }
-        })
+        playAgain();
     }
 }
 
+
 reset();
+
+
+var playAgain = function() {
+    inquirer.prompt([
+        {
+        name: "restart",
+        type: "confirm",
+        message: "Play Again?"
+        }
+    ]).then(function(input) {
+        if (input.restart) {
+            reset();
+        }
+        else {
+            console.log("GAME OVER")
+        }
+    })
+}
