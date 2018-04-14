d8bf0254c57c69d1d17cb65da3af924662d2593c
Jeff Crawford <jeffrey.a.crawford@gmail.com>
2018-04-14 07:49:18 -0400

    bug fixes

--- Index.js
+++ Index.js
@@ -1,28 +1,39 @@
+/* EXTERNAL DOCUMENT VARIABLES */
 var Word = require("./word.js");
 var Letter = require("./letter.js");
 var inquirer = require("inquirer");
 
 
+/* WORDLIST VARIABLES */
 var wordList = ["astrophysics", "astronomy", "atom", "beaker", "biochemistry", "biology", "botany", "cell", "chemical", "chemistry", "climate", "climatologist", "control", "cuvette", "data", "datum", "electricity", "electrochemist", "element", "energy", "entomology", "evolution", "experiment", "fact", "flask", "fossil", "funnel", "genetics", "geology", "geophysics", "glassware", "gravity", "herpetology", "hypothesis", "ichthyology", "immunology", "lab", "laboratory", "laws", "lepidoptery", "magnetism", "mass", "matter", "measure", "meteorologist", "meteorology", "microbiologist", "microbiology", "microscope", "mineral", "mineralogy", "molecule", "motion", "observe", "observatory", "organism", "ornithology", "paleontology", "particle", "phase", "physics", "pipette", "radiology", "research", "retort", "scale", "science", "scientist", "seismology", "telescope", "temperature", "theory", "thermometer", "tissue", "variable", "virologist", "volcanology", "volume", "weather", "weigh", "zoology"];
 var wordSelection = function() {
     return wordList[Math.floor(Math.random()*wordList.length)];
 }
 
+/* NEW WORD OBJECT VARIABLES */
 var text = wordSelection().toString();
 var letterArray = [];
+
 var newWord = new Word(text, letterArray);
 
 
+/* CYCLES THROUGH THE SELECTED WORD STRING AND GENERATES AN ARRAY OF LETTER OBJECTS */
 var fillArray = function() {
     for (i = 0; i < text.length; i++) {
         var newLetter = new Letter(text[i])
         letterArray.push(newLetter);
     }
 }
+fillArray();
 
 
+/* INPUT VARIABLES USED IN BEGIN FUNCTION */
 var input = process.argv[2];
 var count = 0;
+
+
+/* DISPLAYS INITIAL _ STRING */
+
 
 
 var reset = function() {
@@ -45,51 +56,6 @@ var checkProgress = function() {
     }
 }
 
-/* PROMPTS THE USER TO GUESS A LETTER */
-var begin = function() {
-    if (count < 10) {
-        inquirer.prompt([
-            {
-            name: "guess",
-            message: "Guess a letter!"
-            }
-        ]).then(function(input) {
-            if (newWord.text.includes(input.guess.toLowerCase())) {
-                console.log("\n");
-                console.log("CORRECT!")
-
-
-                
-            }
-            else {
-                console.log("\n");
-                console.log("WRONG!")
-                count++;
-                console.log("\n");
-                console.log(10 - count + " guesses remaining!")
-            }
-            console.log("\n");
-            newWord.returnString(input.guess.toLowerCase());
-
-            
-            
-            checkProgress();
-
-        })
-        
-    }
-    else {
-        console.log("\n");
-        console.log("YOU LOSE!")
-        wordSelection();
-        playAgain();
-    }
-}
-
-
-reset();
-
-
 var playAgain = function() {
     inquirer.prompt([
         {
@@ -106,3 +72,47 @@ var playAgain = function() {
         }
     })
 }
+
+/* PROMPTS THE USER TO GUESS A LETTER */
+var begin = function() {
+    if (count < 30) {
+        inquirer.prompt([
+            {
+            name: "guess",
+            message: "Guess a letter!"
+            }
+        ]).then(function(input) {
+            /* IF THE WORD INCLUDES THE GUESS, LOG CORRECT AND CHECK IF THEY'VE WON */
+            if (newWord.text.includes(input.guess.toLowerCase())) {
+                console.log("\n");
+                console.log("CORRECT!");
+                
+            }
+            /* IF THE WORD DOES NOT INCLUDE THE GUESS, LOG WRONG AND ADD TO THE MISS COUNT */
+            else {
+                console.log("\n");
+                console.log("WRONG!");
+                count++;
+                console.log("\n");
+                console.log(10 - count + " guesses remaining!");
+            }
+            console.log("\n");
+            newWord.returnString(input.guess.toLowerCase());
+            checkProgress();
+            
+            
+        })
+    }
+    else {
+        console.log("\n");
+        console.log("YOU LOSE!");
+        wordSelection();
+        playAgain();
+    }
+}
+
+
+
+
+reset();
+
